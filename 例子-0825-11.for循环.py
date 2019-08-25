d665c32cee2d002f94a150e9cc89de1573b3615c
blackplusy <osimeno@163.com>
2019-08-25 16:56:53 +0800

    Add files via upload

---
+++ 例子-0825-11.for循环.py
@@ -0,0 +1,21 @@
+#coding=utf-8
+# str1='python is-n,o.1'
+# for i in str1:
+#     print(i)
+
+l=['5kong','bajie','2dog','nezha','taiyi']
+for i in l:
+    for j in i:
+        print(j)
+    print('************')
+
+#range()函数
+#range(10)     0-9
+#range(1,10)   1-9
+
+for i in range(10):
+    print(i)
+print('**********')
+for i in range(1,10):
+    print(i)
+
---
+++ 例子-0825-12.while循环.py
@@ -0,0 +1,12 @@
+#coding=utf-8
+
+a=0
+
+while a<10:
+    a+=1   #a=a+1
+    if a==6:
+        print('sixsixsix!!')
+    print(a)
+
+while True:
+    print('黑哥真帅！！！')
