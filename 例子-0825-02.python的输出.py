166c07c542c3ebfea96e92b0887c8b691f15316a
blackplusy <osimeno@163.com>
2019-08-25 10:55:00 +0800

    Add files via upload

---
+++ 例子-0825-02.python的输出.py
@@ -0,0 +1,55 @@
+#coding=utf-8
+#字符编码(翻译官)
+#utf-8    国际通用字符集
+#GBK2312  汉语字符集
+
+#1.直接输出通过print()函数进行打印
+print('翻滚吧！牛宝宝！')
+
+print(123)
+
+#2.变量输出
+name='python'
+print(name)
+name='gaga'
+print(name)
+
+a=10
+b=20
+print(a+b)
+
+a='20'
+b='30'
+print(a+b)
+
+#3.函数输出
+#len()    计算字符串元素个数
+#abs()    绝对值
+name='test123'
+print(len(name))
+print(abs(-10))
+
+#格式化输出
+'''
+%s   输出字符串
+%d   输出整型
+'''
+
+name='python'
+num=1
+print('%s is %d '%(name,num))
+#print('%s is %d '%(num,name))
+
+name='heygor'
+print('%s is so 666'% name)
+
+
+
+
+
+
+
+
+
+
+
---
+++ 例子-0825-03.python的输入.py
@@ -0,0 +1,8 @@
+#coding=utf-8
+#输入调用input(),括号中可以填写提示信息
+name=input()
+print(name)
+
+name=input('请输入您的法号')
+print('%s 你终于来了！！！'% name )
+
