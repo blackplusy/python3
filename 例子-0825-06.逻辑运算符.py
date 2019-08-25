dcf28270f2d4b57489665ae927f3b5378e97cc95
blackplusy <osimeno@163.com>
2019-08-25 14:28:12 +0800

    Add files via upload

---
+++ 例子-0825-06.逻辑运算符.py
@@ -0,0 +1,16 @@
+#coding=utf-8
+
+
+print(1>2 and 2<3)
+
+#真 and 假==假
+
+print(1>0 or 2<0)
+
+#假 or 真==真
+
+print(not(1<0))
+
+#非假==真
+
+
---
+++ 例子-0825.07.成员运算符.py
@@ -0,0 +1,16 @@
+#coding=utf-8
+
+name='hello world!!!'
+
+for i in name:
+    print(i)
+
+l=['kobe','rose','KG']
+
+for i in l:
+    print(i)
+
+if 'james' in l:
+    print('OK!!!')
+else:
+    print('WTF')
